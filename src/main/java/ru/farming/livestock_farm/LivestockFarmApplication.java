9ffd46493eaf356a59eeac5249e93e99fbd56a89
Terekhoff Sasha <terekhoffag@gmail.com>
2025-10-13 14:32:23 +0400

    init commit

---
+++ src/main/java/ru/farming/livestock_farm/LivestockFarmApplication.java
@@ -0,0 +1,13 @@
+package ru.farming.livestock_farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivestockFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivestockFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/farming/livestock_farm/LivestockFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.farming.livestock_farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestockFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
